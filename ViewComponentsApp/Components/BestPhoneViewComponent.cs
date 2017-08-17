7990ded3bc5e3ece7adc4769bd1562a09be0405c
nickKos <nickKos@NICKKOS>
2017-08-17 23:49:30 +0300

    first_com

---
+++ ViewComponentsApp/Components/BestPhoneViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ViewComponentsApp.Components
+{
+    public class BestPhoneViewComponent
+    {
+    }
+}
