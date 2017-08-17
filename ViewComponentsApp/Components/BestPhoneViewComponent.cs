c319d2fc6cd8c822da41512f40be8349538e013e
nickKos <nickKos@NICKKOS>
2017-08-17 23:52:35 +0300

    fir_fix

--- ViewComponentsApp/Components/BestPhoneViewComponent.cs
+++ ViewComponentsApp/Components/BestPhoneViewComponent.cs
@@ -1,11 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using ViewComponentsApp.Models;
 
 namespace ViewComponentsApp.Components
 {
     public class BestPhoneViewComponent
     {
+        List<Phone> phones;
+        public BestPhoneViewComponent()
+        {
+            phones = new List<Phone>
+            {
+                new Phone {Title="iPhone 7", Price=56000},
+                new Phone {Title="Idol S4", Price=26000 },
+                new Phone {Title="Elite x3", Price=55000 },
+                new Phone {Title="Honor 8", Price=23000 }
+            };
+        }
+        public string Invoke()
+        {
+            var item = phones.OrderByDescending(x => x.Price).Take(1).FirstOrDefault();
+
+            return $"Самый дорогой телефон: {item.Title}  Цена: {item.Price}";
+        }
     }
 }
